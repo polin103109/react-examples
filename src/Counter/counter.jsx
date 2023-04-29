367b01fa59a24fcb832e4a573c84a7f8c83ab7f1
polin103109 <sabrinpolinece18@gmail.com>
2023-04-30 02:05:06 +0600

    updated

--- src/Counter/counter.jsx
+++ src/Counter/counter.jsx
@@ -2,25 +2,20 @@ import { useState } from "react";
 
 function Counter(){
     const[count,setCount] = useState(0);
-    console.log("rendering.....");
     function onIncrement(){
-        console.log("clicked increment button");
-       setCount(count+1);
-       console.log('---->>',count)
-       
-    }
-   
-    return (
-   
-    <div className="counter-wrap">
-         {console.log("rendered")};
-        <h1>{`Counter App:${count}`}</h1>
+        console.log("updating count",new Date());
+    setCount(count+1);
+    // document.getElementById('output').innerText=`current count:${count}`;
+    // count++;
+    console.log(count, new Date());
+     }
+   return (
+       <div className="counter-wrap">
+        {console.log('rendering', new Date())}
+        <h1>Counter App</h1>
         <button onClick={onIncrement}>increment</button>
-        <p>Current Count:</p>
+        <p id="output">{`current count:${count}`}</p>
     </div>
     )
-    
-   
-    
 }
 export default Counter;
