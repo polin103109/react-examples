d44d2656e57f381764dbd94bb183a0580b51a45e
polin103109 <sabrinpolinece18@gmail.com>
2023-05-02 22:31:26 +0600

    extract component and use it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+function StudentIntro() {
+  return (
+    <div>
+      <h1>Name:Sabrina Sharmin</h1>
+      <h2>Institute:RUET</h2>
+      <h3>Department:ECE</h3>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Name:Sabrina Sharmin</h1>
-      <h2>Institute:RUET</h2>
-      <h3>Department:ECE</h3>
+      <StudentIntro />;
     </div>
   );
 }
