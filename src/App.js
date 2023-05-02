6ff1ba69c9d91a04c61323fb294b36dca0e584ee
polin103109 <sabrinpolinece18@gmail.com>
2023-05-02 22:19:44 +0600

    update component with custom data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div className="App">
+      <h1>Name:Sabrina Sharmin</h1>
+      <h2>Institute:RUET</h2>
+      <h3>Department:ECE</h3>
+    </div>
+  );
 }
 
 export default App;
