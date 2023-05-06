c4564c50d9b04f1921486dc110ae8e2515ab0d6d
polin103109 <sabrinpolinece18@gmail.com>
2023-05-06 16:55:59 +0600

    implement using async await

--- src/Entries/Entry.jsx
+++ src/Entries/Entry.jsx
@@ -4,16 +4,16 @@ import './Entries.css';
 export default function Entries(){
     const[entries,setEntries]= useState([]);
     useEffect(()=>{
-        fetch('https://api.publicapis.org/entries')
-        .then((res)=>res.json())
-        .then((data)=>{
-            setEntries(data.entries.slice(0,10));
-            console.log(data.entries.slice(0,10));
-        })
-        .catch(err=>{
-            alert('failed to load data');
-            console.error(err);
-    });
+    const fetchData = async() => {
+      try{const res = await fetch('https://api.publicapis.org/entries');
+        const data =await res.json();
+        setEntries(data.entries.slice(0,10));
+    } catch(err){
+        alert('failed to load data');
+        console.error(err);
+    }
+    };
+    fetchData();
     },[])
     return <div className="entries">
         <ul>{entries.map((entry,index)=>(
