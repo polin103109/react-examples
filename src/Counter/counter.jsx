2c45360b1f8e75378c2ba65043e683b350a39f70
polin103109 <sabrinpolinece18@gmail.com>
2023-05-06 13:40:27 +0600

    add useEffect hook and it's gonna execute with every single render

--- src/Counter/counter.jsx
+++ src/Counter/counter.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Counter(){
     const[count,setCount] = useState(0);
+    useEffect(()=>{
+console.log("executing use effect");
+    },
+    );
     function onIncrement(){
-        console.log("updating count",new Date());
+      
     setCount(count+1);
-    // document.getElementById('output').innerText=`current count:${count}`;
-    // count++;
-    console.log(count, new Date());
-     }
+    }
+    
+     console.log("rendering...............");
    return (
        <div className="counter-wrap">
-        {console.log('rendering', new Date())}
         <h1>Counter App</h1>
         <button onClick={onIncrement}>increment</button>
         <p id="output">{`current count:${count}`}</p>
