fe390c0a1a130e4a08f7ed12670160dd5038c276
polin103109 <sabrinpolinece18@gmail.com>
2023-05-13 22:37:58 +0600

    props pass down to child component

--- src/Profile/PersonName.jsx
+++ src/Profile/PersonName.jsx
@@ -1,9 +1,12 @@
+const UserPortfolio = ({url,firstname,lastname}) => {
+   return <a href={url}>{`Name: ${firstname} ${lastname}`}</a>
+}
 
-
-export const PersonName = ({firstname,lastname}) =>{
+export const PersonName = ({firstname,lastname,url}) =>{
     return (
         <div>
              <h1>{`Name: ${firstname} ${lastname}`}</h1>
+             <UserPortfolio firstname={firstname} lastname={lastname} url={url}/>
         </div>
     )
 }
--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -6,7 +6,7 @@ const PersonAge = ({age}) =>  <h2>Age: {age}</h2>;
 
 export default function Profile ({user}){
     return <div>
-       <PersonName firstname={user.firstname} lastname={user.lastname}/>
+       <PersonName firstname={user.firstname} lastname={user.lastname} url={user.url}/>
        <PersonAge age={user.age}/>
     </div>
 }
