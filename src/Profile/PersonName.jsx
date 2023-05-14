235a111c9479c0d0c2339ce7271d808b15d562b8
polin103109 <sabrinpolinece18@gmail.com>
2023-05-14 20:37:09 +0600

    use context in class based component

--- src/Profile/PersonName.jsx
+++ src/Profile/PersonName.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import React, { useContext } from "react"
 import UseContext from "../context/UserContext"
 
 const UserPortfoliolink = () => {
@@ -6,12 +6,26 @@ const UserPortfoliolink = () => {
    return <a href={url}>{`Name: ${firstname} ${lastname}`}</a>
 }
 
-export const PersonName = () =>{
-    const {firstname,lastname}=useContext(UseContext);
+// export const PersonName = () =>{
+//     const {firstname,lastname}=useContext(UseContext);
+//     return (
+//         <div>
+//              <h1>{`Name: ${firstname} ${lastname}`}</h1>
+//              <UserPortfoliolink firstname={firstname} lastname={lastname} />
+//         </div>
+//     )
+// }
+class PersonName extends React.Component{
+ 
+  render(){
+    const { firstname,lastname }=this.context;
     return (
-        <div>
-             <h1>{`Name: ${firstname} ${lastname}`}</h1>
-             <UserPortfoliolink firstname={firstname} lastname={lastname} />
-        </div>
-    )
+             <div>
+              <h1>{`Name: ${firstname} ${lastname}`}</h1>
+                  <UserPortfoliolink firstname={firstname} lastname={lastname} />
+                 </div>
+         );
+  }
 }
+PersonName.contextType= useContext;
+export {PersonName};
